04a4e81ba5a09b7f8590ac64bf7e773059c3d142
home <cz2123zc@gmail.com>
2025-02-19 19:54:38 +0900

    버그픽스

--- app/models/user.py
+++ app/models/user.py
@@ -1,17 +1,17 @@
 from pydantic import EmailStr
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime, UTC
 
 from database import Base
 
 class User(Base):
   __tablename__ = "user"
-  id: Mapped[int] = Column(Integer, primary_key=True, index=True)
-  email:Mapped[EmailStr] = Column(EmailStr, unique=True, index=True)
-  nickname:Mapped[str] = Column(String, index=True)
-  hashed_password:Mapped[str] = Column(String, nullable=False)
-  is_active:Mapped[bool] = Column(Boolean, default=True)
-  is_superuser: Mapped[bool] = Column(Boolean, default=False)
-  created_at: Mapped[datetime] = Column(DateTime, default=datetime.now(UTC))
-  updated_at: Mapped[datetime] = Column(DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC))
+  id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+  email: Mapped[EmailStr] = mapped_column(String, unique=True, index=True)
+  nickname:Mapped[str] = mapped_column(String, index=True)
+  hashed_password:Mapped[str] = mapped_column(String, nullable=False)
+  is_active:Mapped[bool] = mapped_column(Boolean, default=True)
+  is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+  created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+  updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC))
